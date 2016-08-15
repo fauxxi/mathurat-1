92ac4f84af1974882389a7eb2d83548f47201680
ansaril3 <ansaril3g@gmail.com>
2016-08-15 17:36:04 +0300

    initial

---
+++ app/pages/info/info.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the InfoPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/info/info.html',
+})
+export class InfoPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
