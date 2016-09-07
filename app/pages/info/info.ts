b242e689904fcb01de3e2036366ef7ded10efe87
ansaril3 <ansaril3g@gmail.com>
2016-09-07 12:47:25 +0300

    обратная cdzpm email

--- app/pages/info/info.ts
+++ app/pages/info/info.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+import {Component} from '@angular/core';
+import { EmailComposer } from 'ionic-native';
 
 /*
   Generated class for the InfoPage page.
@@ -11,8 +12,39 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/info/info.html',
 })
 export class InfoPage {
+  mailText: any;
+  platform: any;
+  tmp: any;
+  constructor(public nav: NavController, platform: Platform) {
+    this.mailText = "";
+    this.platform = platform;
+   // console.log("mail init");
+  }
 
-  constructor(private navCtrl: NavController) {
+  sendMail() {
+   // console.log("test mail");
+    this.platform.ready().then(() => {
+      console.log("platform.ready");
+      this.tmp = EmailComposer.isAvailable().then((available: boolean) => {
+        console.log(available);
+        
+         // console.log(" mail available");
+          let email = {
+            to: 'ansaril3g@gmail.com',
+
+         //   bcc: ['ansaril3g@gmail.com'],
+            attachments: [],
+            subject: 'Приложение Al-Mathurat Rus',
+            body: this.mailText,
+            isHtml: false
+          };
+
+          // Send a text message using default options
+          EmailComposer.open(email);
+        
+      });
+    });
+
 
   }
 
